2c710445fc7515e3f5490800f8320e0b910be90c
Josuel Santos <josuelsantos404@gmail.com>
2022-11-10 07:13:45 -0300

    Project created

---
+++ AppWebMvcSales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWebMvcSales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
